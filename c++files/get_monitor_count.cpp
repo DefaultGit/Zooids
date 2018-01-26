1e27bc6849bf1778a831527a03ca0aef169990ae
DefaultGit <33762472+DefaultGit@users.noreply.github.com>
2018-01-26 20:21:55 +0100

    from internet

---
+++ c++files/get_monitor_count.cpp
@@ -0,0 +1,28 @@
+#include<Windows.h>
+#include<iostream>
+
+#include<direct.h>
+
+using namespace std;
+
+
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
+{
+    int *Count = (int*)dwData;
+    (*Count)++;
+    return TRUE;
+}
+
+int MonitorCount()
+{
+    int Count = 0;
+    if (EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, (LPARAM)&Count))
+        return Count;
+    return -1;//signals an error
+}
+
+
+    int main()
+{
+cout << MonitorCount();
+}
