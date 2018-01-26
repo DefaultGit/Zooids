b32960e89c2003915357659db595cd0613408996
DefaultGit <33762472+DefaultGit@users.noreply.github.com>
2018-01-26 18:33:48 +0100

    updated grid to show uncomplete squares
    also added two lines to be  able to count pixels on the projected image

--- MatLab/Grid.m
+++ MatLab/Grid.m
@@ -2,74 +2,49 @@
 clear
 close all
 
-%% ~~~~~~~~~~ Screen/Monitor/TV ~~~~~~~~~~ %%
-WIDTH = 1;
-HEIGHT = 2;
+%% ~~~~~~~~~~ Physical Properties of Screen ~~~~~~~~~~ %%
+WIDTH = 1; HEIGHT = 2; % constans
 scrnNumOfPixel = [3840, 2160]; 
 
-screenWidth = 1.21;
-screenHeight = 0.68;
+scrnWidth = 1.21; scrnHeight = 0.68;
 
-scrnPixelWidth = screenWidth / scrnNumOfPixel(WIDTH);
-scrnPixelHeight = screenHeight / scrnNumOfPixel(HEIGHT);
-
-%% ~~~~~~~~~~ Convert to double for faster calculation ~~~~~~~~~~ %%
-scrnPixelWidth = double (scrnPixelWidth)
-scrnPixelHeight = double (scrnPixelHeight)
+scrnPixelWidth = double(scrnWidth / scrnNumOfPixel(WIDTH));
+scrnPixelHeight = double(scrnHeight / scrnNumOfPixel(HEIGHT));
 
 %% ~~~~~~~~~~ Map Input to Output ~~~~~~~~~~ %%
 output = uint8(255*ones(scrnNumOfPixel(HEIGHT),scrnNumOfPixel(WIDTH)));
+outFig = figure;
 
 xCm=1;
 yCm=1;
-
-outFig = figure;
-
 scale = 0.1;
+fullSquareHeight = floor(scrnHeight/scale);
+fullSquareWidth = floor(scrnWidth/scale)
 
-% ASSUMES THAT THE TV PIXELS ARE ALAYS SMALLER THEN THE PROJECTOR PIXELS
 for yTV = 1:1:(scrnNumOfPixel(HEIGHT))
+    for xTV = 1:1:(scrnNumOfPixel(WIDTH))
         yCm = ceil(((yTV -0.5)*scrnPixelHeight) / scale);
-        for xTV = 1:1:(scrnNumOfPixel(WIDTH))
-                xCm = ceil(((xTV - 0.5) * scrnPixelWidth)/scale);
-                if mod(xCm+yCm,2)==1
-                    output(yTV,xTV) = 0;
-                end
+        xCm = ceil(((xTV - 0.5) * scrnPixelWidth)/scale);
+        if (((xTV - 0.5) * scrnPixelWidth) > fullSquareWidth * scale) || (((yTV - 0.5) * scrnPixelHeight) > fullSquareHeight * scale)
+            output(yTV,xTV) = 195 - mod(xCm+yCm,2)*68;
+        else
+        if mod(xCm+yCm,2)==1
+            output(yTV,xTV) = 0;
+            end
         end
+    end
 end
+
+output = addLines(output);
+
+output = flipud(output);
+%output = fliplr(output);
+
 
 imagesc(output);
 imwrite(output, 'grid.png')
 
-%% ~~~~~~~~~ Functions ~~~~~~~~~~ %%
-function inch = mToInch(meter)
-    inch = meter/0.0254;
-end
-function meter = inchToMeter(inch)
-    meter = inch*0.0254;
-end
-
-function diag = fHeightToDiag(AR, height)
-    diag = sqrt(1+sym(AR)^2)*sym(height);
-end
-
-function diag = heightToDiag(AR, height)
-    diag = double(fHeightToDiag(AR,height));
-end
-
-function diag = fWidthToDiag(AR, height)
-    diag = sqrt(1+sym(AR)^2)*sym(height);
-end
-
-function diag = widthToDiag(AR, height)
-    diag = double(fWidthToDiag(AR,height));
-end
-
-function [width, height] = fDiagToRectangular(AR, diag)
-    width = (sym(diag)/sqrt(1+(1/sym(AR))^2));
-    height = (sym(diag)/sqrt(1+(sym(AR))^2));
-end
-
-function [width, height] = diagToRectangular(AR, diag)
-    [width, height] = double(fDiagToRectangular(AR, diag));
+function picture = addLines(picture)
+    picture(20,5:15)= 0;
+    picture(5:15,20)= 0;
 end
